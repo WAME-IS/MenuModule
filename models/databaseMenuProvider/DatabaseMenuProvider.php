ca29b4289c2412b2a21ea32116c7d21873306ede
MacMini2 <djsucho@gmail.com>
2016-05-10 16:17:07 +0200

    add DatabaseMenuProvider

---
+++ models/databaseMenuProvider/DatabaseMenuProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Wame\MenuModule\Models;
+
+use Nette\Security\User;
+use Wame\ComponentModule\Repositories\ComponentRepository;
+use Wame\MenuModule\Repositories\MenuRepository;
+use Wame\MenuModule\Models\MenuItemProcessor;
+
+class DatabaseMenuProvider
+{	
+	/** @var User */
+	private $user;
+	
+	/** @var ComponentRepository */
+	private $componentRepository;
+	
+	/** @var MenuRepository */
+	private $menuRepository;
+	
+	/** @var MenuItemProcessor */
+	private $menuItemProcessor;
+	
+	/** @var string */
+	private $name;
+	
+	
+	public function __construct(
+		User $user, 
+		ComponentRepository $componentRepository,
+		MenuRepository $menuRepository,
+		MenuItemProcessor $menuItemProcessor
+	) {
+		$this->user = $user;
+		$this->componentRepository = $componentRepository;
+		$this->menuRepository = $menuRepository;
+		$this->menuItemProcessor = $menuItemProcessor;
+	}
+	
+	
+	/**
+	 * Set component name
+	 * 
+	 * @param string $name
+	 * @return \Wame\MenuModule\Models\DatabaseMenuProvider
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+		
+		return $this;
+	}
+	
+
+    /**
+     * Get menu items from database
+     * 
+     * @return array
+     */
+    public function getItems()
+    {
+		$component = $this->componentRepository->get(['name' => $this->name]);
+		
+		if ($this->user->isLoggedIn()) {
+			$showing = 1;
+		} else {
+			$showing = null;
+		}
+		
+		$items = $this->menuRepository->getItems($component, $showing);
+		
+        return $this->menuItemProcessor->process($items);
+    }
+
+}
---
+++ models/databaseMenuProvider/MenuItemProcessor.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Wame\MenuModule\Models;
+
+use Wame\MenuModule\Models\Item;
+use Wame\MenuModule\Repositories\MenuRepository;
+use Wame\MenuModule\Models\MenuManager;
+
+class MenuItemProcessor
+{	
+	/** @var string */
+	private $lang;
+	
+	/** @var array */
+	private $menuManager;
+	
+	
+	public function __construct(MenuRepository $menuRepository, MenuManager $menuManager) 
+	{
+		$this->lang = $menuRepository->lang;
+		$this->menuManager = $menuManager->menuItemTypes;
+	}
+	
+	
+	public function process($databaseItems)
+	{
+		$items = [];
+		
+		foreach ($databaseItems as $item) {
+			$items[0][] = $this->addItem($item);
+		}
+		
+		return $items;
+	}
+	
+	
+	private function addItem($item) 
+	{
+		$menuItem = new Item();
+		$menuItem->setName('item_' . $item->id);
+		$menuItem->setTitle($item->langs[$this->lang]->getItemTitle());
+		$menuItem->setLink($this->menuManager[$item->type]->getLink($item));
+		
+		if ($item->getParameter('icon')) {
+			$menuItem->setIcon($item->getParameter('icon'));
+		}
+		
+		if ($item->getParameter('class')) {
+			$menuItem->addAttributes(['class' => $item->getParameter('class')]);
+		}
+		
+		return $menuItem->getItem();
+	}
+
+}
