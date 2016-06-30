3208e8e7e4776996f572556fda1e87ebc1ba07e3
Linzee <d.gmiterko@gmail.com>
2016-06-30 11:38:00 +0200

    Unified utility getClassName methods

--- models/DatabaseMenuProvider/MenuManager.php
+++ models/DatabaseMenuProvider/MenuManager.php
@@ -4,82 +4,79 @@ namespace Wame\MenuModule\Models;
 
 use Wame\MenuModule\Models\IMenuProvider;
 use Wame\MenuModule\Models\ItemSorter;
+use Wame\Utils\Strings;
 
 class MenuManager implements IMenuProvider
-{	
-	/** @var array */
-	public $menuItemTypes = [];
-	
-	/** @var array */
-	private $removeMenuItemType = [];
-	
-	/** @var ItemSorter */
-	private $itemSorter;
-	
-	
-	public function __construct(ItemSorter $itemSorter) 
-	{
-		$this->itemSorter = $itemSorter;
-	}
-	
-	
-	/**
-	 * Add menu item type
-	 * 
-	 * @param object $menuItemType
-	 * @param string $name
-	 * @return \Wame\MenuModule\Models\MenuManager
-	 */
-	public function addMenuItemType($menuItemType, $name = null)
-	{
-		if (!$name) {
-			$name = $this->getClassName($menuItemType);
-		}
-		
-		$this->menuItemTypes[$name] = $menuItemType;
-		
-		return $this;
-	}
-	
-	
-	/**
-	 * Add menu item type to remove list
-	 * 
-	 * @param mixed $menuItemType
-	 * @return \Wame\MenuModule\Models\MenuManager
-	 */
-	public function removeMenuItemType($menuItemType)
-	{
-		if (is_object($menuItemType)) {
-			$name = $this->getClassName($menuItemType);
-		} else {
-			$name = $menuItemType;
-		}
-		
-		$this->removeMenuItemType[$name] = $name;
-		
-		return $this;
-	}
-	
-	
-	/**
-	 * Remove menu item types
-	 * 
-	 * @return array
-	 */
-	private function removeMenuItemTypes()
-	{
-		$menuItemTypes = $this->menuItemTypes;
-		
-		foreach ($this->removeMenuItemType as $menuItemType) {
-			if (array_key_exists($menuItemType, $menuItemTypes)) {
-				unset($menuItemTypes[$menuItemType]);
-			}
-		}
-		
-		return $menuItemTypes;
-	}
-	
+{
+
+    /** @var array */
+    public $menuItemTypes = [];
+
+    /** @var array */
+    private $removeMenuItemType = [];
+
+    /** @var ItemSorter */
+    private $itemSorter;
+
+    public function __construct(ItemSorter $itemSorter)
+    {
+        $this->itemSorter = $itemSorter;
+    }
+
+    /**
+     * Add menu item type
+     * 
+     * @param object $menuItemType
+     * @param string $name
+     * @return MenuManager
+     */
+    public function addMenuItemType($menuItemType, $name = null)
+    {
+        if (!$name) {
+            $name = Strings::getClassName($menuItemType);
+        }
+
+        $this->menuItemTypes[$name] = $menuItemType;
+
+        return $this;
+    }
+
+    /**
+     * Add menu item type to remove list
+     * 
+     * @param mixed $menuItemType
+     * @return MenuManager
+     */
+    public function removeMenuItemType($menuItemType)
+    {
+        if (is_object($menuItemType)) {
+            $name = Strings::getClassName($menuItemType);
+        } else {
+            $name = $menuItemType;
+        }
+
+        $this->removeMenuItemType[$name] = $name;
+
+        return $this;
+    }
+
+    /**
+     * Remove menu item types
+     * 
+     * @return array
+     */
+    private function removeMenuItemTypes()
+    {
+        $menuItemTypes = $this->menuItemTypes;
+
+        foreach ($this->removeMenuItemType as $menuItemType) {
+            if (array_key_exists($menuItemType, $menuItemTypes)) {
+                unset($menuItemTypes[$menuItemType]);
+            }
+        }
+
+        return $menuItemTypes;
+    }
 
     /**
      * Get menu item types from services
@@ -88,23 +85,8 @@ class MenuManager implements IMenuProvider
      */
     public function getItems()
     {
-		$menuItemTypes = $this->removeMenuItemTypes();
-		
+        $menuItemTypes = $this->removeMenuItemTypes();
+
         return $this->itemSorter->sort($menuItemTypes);
     }
-	
-	
-	/**
-	 * Get class name from namespace
-	 * 
-	 * @param string $namespace
-	 * @return string
-	 */
-	public function getClassName($namespace)
-	{
-		$reflect = new \ReflectionClass($namespace);
-		
-		return $reflect->getShortName();
-	}
-
 }
