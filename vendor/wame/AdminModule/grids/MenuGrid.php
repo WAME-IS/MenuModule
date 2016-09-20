81653a4d4014f3bd4a10cbf2b4de370aacfb6960
Stanislav Červeňák <stano@wame.sk>
2016-09-20 12:32:29 +0200

    EDIT DataGrids

--- vendor/wame/AdminModule/grids/MenuGrid.php
+++ vendor/wame/AdminModule/grids/MenuGrid.php
@@ -2,7 +2,7 @@
 
 namespace Wame\MenuModule\Vendor\Wame\AdminModule\Grids;
 
-class MenuGrid extends \Wame\DataGridControl\DataGridControl
+class MenuGrid extends \Wame\AdminModule\Vendor\Wame\DataGridControl\AdminDataGridControl
 {
     
 }
