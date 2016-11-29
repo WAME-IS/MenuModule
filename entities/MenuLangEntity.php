94095c1b2cfad8822e3b2b29e99f71c9dad3f44d
Stanislav Červeňák <stano@wame.sk>
2016-11-29 13:57:36 +0100

    FIX webalize ExternalLink slug

--- entities/MenuLangEntity.php
+++ entities/MenuLangEntity.php
@@ -5,6 +5,8 @@ namespace Wame\MenuModule\Entities;
 use Doctrine\ORM\Mapping as ORM;
 use Wame\Core\Entities\Columns;
 use Wame\Core\Entities\BaseLangEntity;
+use Wame\Utils\Strings;
+
 
 /**
  * @ORM\Table(name="wame_menu_lang")
@@ -17,7 +19,6 @@ class MenuLangEntity extends BaseLangEntity
 	use Columns\EditUser;
 	use Columns\Lang;
 	use Columns\Title;
-	use Columns\Slug;
 	use Columns\Parameters;
 
 	/**
@@ -25,25 +26,25 @@ class MenuLangEntity extends BaseLangEntity
      * @ORM\JoinColumn(name="item_id", referencedColumnName="id", nullable=false)
      */
 	protected $item;
-	
+
 	/**
 	 * @ORM\Column(name="alternative_title", type="string", nullable=true)
 	 */
 	protected $alternativeTitle;
 
-	
-	public function getAlternativeTitle()
+    /**
+	 * @ORM\Column(name="slug", type="string", nullable=true)
+	 */
+	protected $slug;
+
+
+	/** get ************************************************************/
+
+    public function getAlternativeTitle()
 	{
 		return $this->alternativeTitle;
 	}
-	
-	public function setAlternativeTitle($alternativeTitle)
-	{
-		$this->alternativeTitle = $alternativeTitle;
-		
-		return $this;
-	}
-	
+
 	public function getItemTitle()
 	{
 		if ($this->getAlternativeTitle()) {
@@ -52,12 +53,37 @@ class MenuLangEntity extends BaseLangEntity
 			return $this->getTitle();
 		}
 	}
-    
-    
+
+    public function getSlug()
+	{
+		return $this->slug;
+	}
+
+
+	/** set ************************************************************/
+
+	public function setAlternativeTitle($alternativeTitle)
+	{
+		$this->alternativeTitle = $alternativeTitle;
+
+		return $this;
+	}
+
+    public function setSlug($slug)
+	{
+        if ($this->item->getType() == 'externalLink') {
+            $this->slug = $slug;
+        } else {
+            $this->slug = Strings::webalize($slug);
+        }
+
+		return $this;
+	}
+
     /** {@inheritDoc} */
     public function setEntity($entity)
     {
         $this->item = $entity;
     }
-	
+
 }
