ad6105a9272373d19c974d210fa511266b1433e8
Stanislav Červeňák <stano@wame.sk>
2016-11-30 15:13:50 +0100

    ADD active link in admin menu

---
+++ vendor/wame/AdminModule/assets/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    // Admin menu active collapse
+    $('.cnt-MenuControl .collapsible-body a.active').closest('.collapsible-body').show().closest('li').addClass('active');
+});
